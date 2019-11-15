d78922f7acdb4ee0eaebd6c125ca566139faec9d
muflih23 <mfadli.muflih23@gmail.com>
2019-11-16 06:30:16 +0700

    Update model karena database diubah

--- application/models/m_login.php
+++ application/models/m_login.php
@@ -3,7 +3,7 @@
 class M_login extends CI_Model{	
 	function cek_login($username){		
 		$this->db->where('username', $username);
-		$result = $this->db->get('admin')->row();
+		$result = $this->db->get('data_akun')->row();
 		return $result;
 	}	
 }
