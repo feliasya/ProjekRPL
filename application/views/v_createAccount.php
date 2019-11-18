ac5667d86a04c4ddf6b68081d7c7c643a9f373e7
muflih23 <mfadli.muflih23@gmail.com>
2019-11-18 20:15:48 +0700

    update error handling agar tidak bisa mengosongkan form pembuatan akun

--- application/views/v_createAccount.php
+++ application/views/v_createAccount.php
@@ -9,8 +9,8 @@
 <div>
     <form action="<?php echo base_url();?>index.php/createAccount/aksi_akunBaru" method="post">
         <span><?php echo $this->session->userdata('message'); ?></span>
-        <label for="fnama">Nama Lengkap</label>
-        <input type="text" id="fnama" name="inputNama" placeholder="Nama Lengkap">
+        <label for="fnama">Nama Lengkap*</label>
+        <input type="text" id="fnama" name="inputNama" placeholder="Nama Lengkap" required="required">
 
         <label for="kontak_hp">Nomor HP</label>
         <input type="text" id="kontak_hp" name="inputHP" placeholder="Nomor Hp">
@@ -18,24 +18,24 @@
         <label for="kontak_email">Email</label>
         <input type="text" id="kontak_email" name="inputEmail" placeholder="Email">
 
-        <label for="lfakutlas">Fakultas</label>
-        <input type="text" id="lfakultas" name="inputFakultas" placeholder="Fakultas">
+        <label for="lfakutlas">Fakultas*</label>
+        <input type="text" id="lfakultas" name="inputFakultas" placeholder="Fakultas" required="required">
 
-        <label for="ljurusan">Program Studi</label>
-        <input type="text" id="ljurusan" name="inputJurusan" placeholder="Program Studi">
+        <label for="ljurusan">Program Studi*</label>
+        <input type="text" id="ljurusan" name="inputJurusan" placeholder="Program Studi" required="required">
 
-        <label for="lnpm">NPM</label>
-        <input type="text" id="lnpm" name="inputNPM" placeholder="NPM">
+        <label for="lnpm">NPM*</label>
+        <input type="text" id="lnpm" name="inputNPM" placeholder="NPM" required="required">
 
-        <label for="langkatan">Angkatan</label>
-        <input type="text" id="langkatan" name="inputAngkatan" placeholder="Angkatan">
+        <label for="langkatan">Angkatan*</label>
+        <input type="text" id="langkatan" name="inputAngkatan" placeholder="Angkatan" required="required">
 
         <p><h5>Gunakan 16 karakter unik yang mudah diingat untuk username anda</h5></p>
-        <label for="uname">Username</label>
-        <input type="text" id="uname" name="inputUname" placeholder="Username">
+        <label for="uname">Username*</label>
+        <input type="text" id="uname" name="inputUname" placeholder="Username" required="required">
         
-        <label for="psswrd">Password</label>
-        <input type="password" id="psswrd" name="inputPw" placeholder="password">
+        <label for="psswrd">Password*</label>
+        <input type="password" id="psswrd" name="inputPw" placeholder="password" required="required">
 
         <input type="submit" value="Buat Akun">
         <p><a href="<?php echo base_url();?>">Cancel</a></p>
