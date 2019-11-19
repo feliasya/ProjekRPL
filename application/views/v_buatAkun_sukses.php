504f2e5681293950857b171b0d83486c670d95cd
feliasya <famsfelia@gmail.com>
2019-11-19 14:25:35 +0700

    buat akun sukses

--- application/views/v_buatAkun_sukses.php
+++ application/views/v_buatAkun_sukses.php
@@ -2,10 +2,69 @@
 <html>
 <head>
 	<title>Akun Berhasil Dibuat!!</title>
+<style type="text/css">
+
+body{
+	background-color:#3E5C76;
+	margin: 0px;
+}
+
+.header {
+  background-color: #F0EBD8;
+  padding: 20px;
+  color: #3E5C76;
+}
+
+h1,h5, a {
+	text-align: center;
+}
+
+div.content{
+  border-radius: 10px;
+  background-color:#F0EBD8;
+  padding-left: 80px;
+  padding-right: 80px;
+  padding-top: 50px;
+  padding-bottom: 50px;
+  margin-top: 50px;
+  margin-left: 400px;
+  margin-right: 400px;
+  color: #3E5C76;
+  font-size: 18px;
+}
+
+img{
+	height: 200px;
+	width: 200px;
+	margin-left: 100px;
+}
+
+a {
+  width: 100%;
+  background-color: white;
+  color: #0D1321;
+  padding: 10px 180px;
+  border: 1px solid #ccc;
+  border-radius: 10px;
+  cursor: pointer;
+  margin-left: 0px;
+}
+
+a:hover {
+  background-color: #0D1321;
+  color:white;
+}
+</style>
 </head>
 <body>
-	<h1>Akun Berhasil Dibuat!!!</h1>
-    <h2>Silahkan login dengan akun anda untuk melanjutkan</h2>
-	<a href="<?php echo base_url();?>">Login</a>
+
+	<div class="header">CheckIn</div>
+
+	<div class="content">
+		<img src="https://archimateriali.com/wp-content/uploads/2018/01/cropped-centang.png">
+		<h1>Create Account Success!</h1>
+	    <h5>Login with your account to continue</h5>
+		<a href="<?php echo base_url();?>">Login</a>
+	</div>
 </body>
 </html>
